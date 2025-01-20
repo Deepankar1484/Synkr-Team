69dd75079c68642b38a295846632b34f8a5da9af
Deepankar Garg <122424189+Deepankar1484@users.noreply.github.com>
2025-01-20 16:16:43 +0530

    added more Time capsule screens

--- Synkr-Team/Files/MainTabBarController.swift
+++ Synkr-Team/Files/MainTabBarController.swift
@@ -11,19 +11,11 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let viewControllers = self.viewControllers {
+            let educationVC = viewControllers[1]
+            educationVC.tabBarItem.title = "Time Capsule"
+            educationVC.tabBarItem.image = UIImage(systemName: "battery.75percent")
+//            educationVC.tabBarItem.selectedImage = UIImage(systemName: "envlpe.fill")
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
